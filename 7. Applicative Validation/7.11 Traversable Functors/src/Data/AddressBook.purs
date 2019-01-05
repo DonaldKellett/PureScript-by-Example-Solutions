529b464673281f6c90ea021b6b866dfa79fd2b68
Donald Sebastian Leung <i.donaldl@me.com>
2019-01-05 16:55:55 +0800

    Add partial solution to Ex 2 of 7.11

---
+++ 7. Applicative Validation/7.11 Traversable Functors/src/Data/AddressBook.purs
@@ -0,0 +1,77 @@
+module Data.AddressBook where
+
+import Prelude
+import Data.Maybe -- Exercise 2
+
+newtype Address = Address
+  { street :: String
+  , city   :: String
+  , state  :: String
+  }
+
+address :: String -> String -> String -> Address
+address street city state = Address { street, city, state }
+
+data PhoneType
+  = HomePhone
+  | WorkPhone
+  | CellPhone
+  | OtherPhone
+
+newtype PhoneNumber = PhoneNumber
+  { "type" :: PhoneType
+  , number :: String
+  }
+
+phoneNumber :: PhoneType -> String -> PhoneNumber
+phoneNumber ty number = PhoneNumber
+  { "type": ty
+  , number: number
+  }
+
+newtype Person = Person
+  { firstName   :: String
+  , lastName    :: String
+  , homeAddress :: Maybe Address -- Exercise 2
+  , phones      :: Array PhoneNumber
+  }
+
+-- Exercise 2
+person :: String -> String -> Maybe Address -> Array PhoneNumber -> Person
+person firstName lastName homeAddress phones =
+  Person { firstName, lastName, homeAddress, phones }
+
+examplePerson :: Person
+examplePerson =
+  person "John" "Smith"
+         (Just $ address "123 Fake St." "FakeTown" "CA") -- Exercise 2
+         [ phoneNumber HomePhone "555-555-5555"
+         , phoneNumber CellPhone "555-555-0000"
+         ]
+
+instance showAddress :: Show Address where
+  show (Address o) = "Address " <>
+    "{ street: " <> show o.street <>
+    ", city: "   <> show o.city <>
+    ", state: "  <> show o.state <>
+    " }"
+
+instance showPhoneType :: Show PhoneType where
+  show HomePhone = "HomePhone"
+  show WorkPhone = "WorkPhone"
+  show CellPhone = "CellPhone"
+  show OtherPhone = "OtherPhone"
+
+instance showPhoneNumber :: Show PhoneNumber where
+  show (PhoneNumber o) = "PhoneNumber " <>
+    "{ type: "   <> show o."type" <>
+    ", number: " <> show o.number <>
+    " }"
+
+instance showPerson :: Show Person where
+  show (Person o) = "Person " <>
+    "{ firstName: "   <> show o.firstName <>
+    ", lastName: "    <> show o.lastName <>
+    ", homeAddress: " <> show o.homeAddress <>
+    ", phones: "      <> show o.phones <>
+    " }"
